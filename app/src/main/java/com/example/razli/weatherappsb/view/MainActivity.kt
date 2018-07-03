d56e5756fc1b53f7b4595ceeb35414550fc86841
Raz <razli1993@hotmail.com>
2018-07-03 14:47:47 +0800

    Starting from scratch again. Following Medium tutorial, https://medium.com/@amitkumar_76187/understanding-model-view-presenter-1d79448d39cd

--- app/src/main/java/com/example/razli/weatherappsb/view/MainActivity.kt
+++ app/src/main/java/com/example/razli/weatherappsb/view/MainActivity.kt
@@ -3,29 +3,9 @@ package com.example.razli.weatherappsb.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.razli.weatherappsb.R
-import com.example.razli.weatherappsb.contract.MainContract
-import com.example.razli.weatherappsb.presenter.MainActivityPresenter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var mPresenter: MainContract.Presenter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Create the presenter
-        mPresenter = MainActivityPresenter(this)
-    }
-
-    override fun initView() {
-        mButton.setOnClickListener { /* Nothing for now */ } // mPresenter.onClikc(view)
-    }
-
-    override fun setViewData(viewData: String) {
-
-        // Set text of TextView
-        mTextView.text = viewData
-    }
 }
